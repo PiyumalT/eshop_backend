5f4649ec45d46cd73886ad036de0766e30a410e8
PiyumalT <piyumal-se19035@stu.kln.ac.lk>
2023-08-30 02:58:01 +0530

    just added some comments

--- routers/genaral_router.js
+++ routers/genaral_router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const handle = require('../handlers/genaral')
 
+//this router is for testing purpose only
+//just send a get request to http://localhost:3000/api/v1/ and see the response
+//or send a post request to http://localhost:3000/api/v1/ and see the response
+
 router.get('/', handle.hello)
 router.post('/', handle.helloPost)  
 
